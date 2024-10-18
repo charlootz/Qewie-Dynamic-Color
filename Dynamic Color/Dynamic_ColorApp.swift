42d625e2c96e528355061d947a98e4dda1494691
charlootz <117485803+charlootz@users.noreply.github.com>
2024-10-18 01:28:19 -0400

    Initial Commit

---
+++ Dynamic Color/Dynamic_ColorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dynamic_ColorApp.swift
+//  Dynamic Color
+//
+//  Created by Joseph Albanese on 10/18/24.
+//
+
+import SwiftUI
+
+@main
+struct Dynamic_ColorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
